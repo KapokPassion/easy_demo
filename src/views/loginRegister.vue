bf2878498c5abbbdc34b1f721eaa403ddef68446
KapokPassion <960914028@qq.com>
2023-04-13 00:27:15 +0800

    Revert "remove register"
    This reverts commit b83e686183e1a684b7337b63a4b8ef73180c8c61.

--- src/views/loginRegister.vue
+++ src/views/loginRegister.vue
@@ -12,6 +12,28 @@
 					</div>
 					<button class="bbutton" @click="login">登录</button>
 				</div>
+				<div class="big-contain" key="bigContainRegister" v-else>
+					<div class="btitle">创建账户</div>
+					<div class="bform">
+						<input type="text" placeholder="用户名" v-model="form.username">
+						<span class="errTips" v-if="existed">* 用户名已经存在！ *</span>
+						<input type="email" placeholder="邮箱" v-model="form.useremail">
+						<input type="password" placeholder="密码" v-model="form.userpwd">
+					</div>
+					<button class="bbutton" @click="register">注册</button>
+				</div>
+			</div>
+			<div class="small-box" :class="{active:isLogin}">
+				<div class="small-contain" key="smallContainRegister" v-if="isLogin">
+					<div class="stitle">你好，朋友!</div>
+					<p class="scontent">开始注册，和我们一起旅行</p>
+					<button class="sbutton" @click="changeType">注册</button>
+				</div>
+				<div class="small-contain" key="smallContainLogin" v-else>
+					<div class="stitle">欢迎回来!</div>
+					<p class="scontent">与我们保持联系，请登录你的账户</p>
+					<button class="sbutton" @click="changeType">登录</button>
+				</div>
 			</div>
 		</div>
 	</div>
